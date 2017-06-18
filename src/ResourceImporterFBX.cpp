ef60b3a55f7448db0e38375199b6ae90a87152ec
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-06-18 12:05:22 -0700

    Register methods.
    `const` methods cannot be registered #6 https://github.com/GodotNativeTools/cpp_bindings/issues/6

--- src/ResourceImporterFBX.cpp
+++ src/ResourceImporterFBX.cpp
@@ -12,49 +12,49 @@
 
 using namespace godot;
 
-String ResourceImporterFBX::get_importer_name() const
+String ResourceImporterFBX::get_importer_name()
 {
 	return "fbx_mesh";
 }
 
-String ResourceImporterFBX::get_visible_name() const
+String ResourceImporterFBX::get_visible_name()
 {
 	return "FBX As Mesh";
 }
 
-int ResourceImporterFBX::get_preset_count() const
+int ResourceImporterFBX::get_preset_count()
 {
 	return 0;
 }
 
-String ResourceImporterFBX::get_preset_name(const int preset) const
+String ResourceImporterFBX::get_preset_name(const int preset)
 {
 	return String();
 }
 
-Array ResourceImporterFBX::get_recognized_extensions() const
+Array ResourceImporterFBX::get_recognized_extensions()
 {
 	Array recognized = Array();
 	recognized.push_back("fbx");
 	return recognized;
 }
 
-Array ResourceImporterFBX::get_import_options(const int preset) const
+Array ResourceImporterFBX::get_import_options(const int preset)
 {
 	return Array();
 }
 
-String ResourceImporterFBX::get_save_extension() const
+String ResourceImporterFBX::get_save_extension()
 {
 	return "mesh";
 }
 
-String ResourceImporterFBX::get_resource_type() const
+String ResourceImporterFBX::get_resource_type()
 {
 	return "ArrayMesh";
 }
 
-bool ResourceImporterFBX::get_option_visibility(const String option, const Dictionary options) const
+bool ResourceImporterFBX::get_option_visibility(const String option, const Dictionary options)
 {
 	return true;
 }
@@ -65,14 +65,14 @@ int ResourceImporterFBX::import(const String source_file, const String save_path
 }
 
 void ResourceImporterFBX::_register_methods() {
-	//register_method("get_importer_name", &ResourceImporterFBX::get_importer_name);
-	//register_method("get_visible_name", &ResourceImporterFBX::get_visible_name);
-	//register_method("get_preset_count", &ResourceImporterFBX::get_preset_count);
-	//register_method("get_preset_name", &ResourceImporterFBX::get_preset_name);
-	//register_method("get_recognized_extensions", &ResourceImporterFBX::get_recognized_extensions);
-	//register_method("get_import_options", &ResourceImporterFBX::get_import_options);
-	//register_method("get_save_extension", &ResourceImporterFBX::get_save_extension);
-	//register_method("get_resource_type", &ResourceImporterFBX::get_resource_type);
-	//register_method("get_option_visibility", &ResourceImporterFBX::get_option_visibility);
-	//register_method("import", &ResourceImporterFBX::import);
+	register_method("get_importer_name", &ResourceImporterFBX::get_importer_name);
+	register_method("get_visible_name", &ResourceImporterFBX::get_visible_name);
+	register_method("get_preset_count", &ResourceImporterFBX::get_preset_count);
+	register_method("get_preset_name", &ResourceImporterFBX::get_preset_name);
+	register_method("get_recognized_extensions", &ResourceImporterFBX::get_recognized_extensions);
+	register_method("get_import_options", &ResourceImporterFBX::get_import_options);
+	register_method("get_save_extension", &ResourceImporterFBX::get_save_extension);
+	register_method("get_resource_type", &ResourceImporterFBX::get_resource_type);
+	register_method("get_option_visibility", &ResourceImporterFBX::get_option_visibility);
+	register_method("import", &ResourceImporterFBX::import);
 }
