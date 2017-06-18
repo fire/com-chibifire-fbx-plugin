b7454904da5540db368193dd2e999ee554c0f0f9
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-06-18 06:22:30 -0700

    Initial commit.

---
+++ sample/main.gd
@@ -0,0 +1,7 @@
+extends Control
+
+# load the SIMPLE library
+onready var data = preload("res://ResourceImporterFBX.gdn").new()
+
+func _on_Button_pressed():
+	$Label.text = "Data = " + data.method()
---
+++ src/ResourceImporterFBX.cpp
@@ -0,0 +1,32 @@
+#include <godot.h>
+
+#include <core/Godot.hpp>
+#include <core/GodotGlobal.hpp>
+#include <Reference.hpp>
+
+using namespace godot;
+
+class ResourceImporterFBX : public GodotScript<Reference> {
+	GODOT_CLASS(ResourceImporterFBX);
+public:
+	ResourceImporterFBX() { }
+
+	void test_void_method() {
+		Godot::print("This is test");
+	}
+
+	Variant method(Variant arg) {
+		Variant ret;
+		ret = arg;
+
+		return ret;
+	}
+
+	static void _register_methods() {
+		register_method("method", &ResourceImporterFBX::method);
+	}
+};
+
+GODOT_NATIVE_INIT(godot_native_init_options *options) {
+	register_class<ResourceImporterFBX>();
+}
