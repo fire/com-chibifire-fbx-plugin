246c1835aaa9c09257a2870da569f5d4fc381251
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-06-18 11:19:37 -0700

    Submit work so it can be shown for review.

--- src/ResourceImporterFBX.cpp
+++ src/ResourceImporterFBX.cpp
@@ -1,3 +1,4 @@
+#include "ResourceImporterFBX.h"
 #include <godot.h>
 
 #include <core/Godot.hpp>
@@ -9,21 +10,68 @@
 
 #include <Reference.hpp>
 
-#include "ResourceImporterFBX.h"
-
 using namespace godot;
 
-inline void ResourceImporterFBX::test_void_method() {
-	Godot::print("This is test");
+
+String ResourceImporterFBX::get_importer_name() const
+{
+	return String();
 }
 
-inline Variant ResourceImporterFBX::method(Variant arg) {
-	Variant ret;
-	ret = arg;
+String ResourceImporterFBX::get_visible_name() const
+{
+	return String();
+}
 
-	return ret;
+int ResourceImporterFBX::get_preset_count() const
+{
+	return 0;
+}
+
+String ResourceImporterFBX::get_preset_name(const int preset) const
+{
+	return String();
+}
+
+Array ResourceImporterFBX::get_recognized_extensions() const
+{
+	return Array();
+}
+
+Array ResourceImporterFBX::get_import_options(const int preset) const
+{
+	return Array();
+}
+
+String ResourceImporterFBX::get_save_extension() const
+{
+	return String();
+}
+
+String ResourceImporterFBX::get_resource_type() const
+{
+	return String();
+}
+
+bool ResourceImporterFBX::get_option_visibility(const String option, const Dictionary options) const
+{
+	return false;
+}
+
+int ResourceImporterFBX::import(const String source_file, const String save_path, const Dictionary options, const Array r_platform_variants, const Array r_gen_files)
+{
+	return 0;
 }
 
 inline void ResourceImporterFBX::_register_methods() {
-	register_method("method", &ResourceImporterFBX::method);
+	//register_method("get_importer_name", &ResourceImporterFBX::get_importer_name);
+	//register_method("get_visible_name", &ResourceImporterFBX::get_visible_name);
+	//register_method("get_preset_count", &ResourceImporterFBX::get_preset_count);
+	//register_method("get_preset_name", &ResourceImporterFBX::get_preset_name);
+	//register_method("get_recognized_extensions", &ResourceImporterFBX::get_recognized_extensions);
+	//register_method("get_import_options", &ResourceImporterFBX::get_import_options);
+	//register_method("get_save_extension", &ResourceImporterFBX::get_save_extension);
+	//register_method("get_resource_type", &ResourceImporterFBX::get_resource_type);
+	//register_method("get_option_visibility", &ResourceImporterFBX::get_option_visibility);
+	//register_method("import", &ResourceImporterFBX::import);
 }
--- src/ResourceImporterFBX.h
+++ src/ResourceImporterFBX.h
@@ -1,25 +1,40 @@
 #pragma once
 
-#include "ResourceImporter.hpp"
+#include <godot.h>
 
-using godot::register_class;
-using godot::GodotScript;
+#include <core/Godot.hpp>
+#include <core/GodotGlobal.hpp>
+#include <Reference.hpp>
+#include "EditorImportPlugin.hpp"
+
+using namespace godot;
 using godot::Reference;
 using godot::Variant;
-using godot::ResourceImporter;
+using godot::EditorImportPlugin;
+using godot::String;
+using godot::Reference;
+using godot::Array;
+using godot::Dictionary;
 
-class ResourceImporterFBX : public GodotScript<Reference> {
+class ResourceImporterFBX : public EditorImportPlugin {
 	GODOT_CLASS(ResourceImporterFBX);
 public:
 	ResourceImporterFBX() { }
-
-	void test_void_method();
-
-	Variant method(Variant arg);
+	
+	String get_importer_name() const;
+	String get_visible_name() const;
+	int get_preset_count() const;
+	String get_preset_name(const int preset) const;
+	Array get_recognized_extensions() const;
+	Array get_import_options(const int preset) const;
+	String get_save_extension() const;
+	String get_resource_type() const;
+	bool get_option_visibility(const String option, const Dictionary options) const;
+	int import(const String source_file, const String save_path, const Dictionary options, const Array r_platform_variants, const Array r_gen_files);
 
 	static void _register_methods();
 };
 
 GODOT_NATIVE_INIT(godot_native_init_options *options) {
-	register_class<ResourceImporterFBX>();
+//	register_class<ResourceImporterFBX>();
 }
