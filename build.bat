b5d40bfe5872d67ef39f2a8e0d48f33fb3ee2c1b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-12-02 09:04:18 -0800

    Update scripts.

--- build.bat
+++ build.bat
@@ -1,8 +1,1 @@
-set current=%cd%
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
-cd /D %current%/Godot
-call scons p=windows target=debug -j%NUMBER_OF_PROCESSORS% use_lto=yes gdnative_wrapper=yes
-cd /D %current%
-call Godot\bin\godot.windows.tools.64.exe --gdnative-generate-json-api CppBindings/godot_api.json
-cd CppBindings
-call scons godotbinpath="../Godot/bin/" headers="../Godot/modules/gdnative/include" p=Windows generate_bindings=yes
+bazel build //:itchio-bin
