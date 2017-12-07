d2861d82b43fad13fdc68081c0593be75fb43a56
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-12-06 22:06:28 -0800

    Update build_dependencies script.

--- build_dependencies.bat
+++ build_dependencies.bat
@@ -1,10 +1,10 @@
 set current=%cd%
-set godot=%current%\..\..\Editor\Godot
+set godot=%current%\thirdparty\godot
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
 cd /D %godot%
 call scons p=windows target=debug -j%NUMBER_OF_PROCESSORS% use_lto=yes gdnative_wrapper=yes
 cd /D %current%
-call %godot%/bin/godot.windows.tools.64.exe --gdnative-generate-json-api CppBindings/godot_api.json
-cd /D CppBindings
+call %godot%/bin/godot.windows.tools.64.exe --gdnative-generate-json-api thirdparty/cpp_bindings/godot_api.json
+cd /D thirdparty/cpp_bindings
 call scons godotbinpath="%godot%/bin/" headers="%godot%/modules/gdnative/include" p=Windows generate_bindings=yes
 cd /D %current%
