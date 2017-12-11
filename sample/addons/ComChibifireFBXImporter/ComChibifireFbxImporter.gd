7c2a9d4f640573120877cb670ed40115d6eda4f4
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-12-10 21:27:42 -0800

    Remove hacks.

--- sample/addons/ComChibifireFBXImporter/ComChibifireFbxImporter.gd
+++ sample/addons/ComChibifireFBXImporter/ComChibifireFbxImporter.gd
@@ -9,9 +9,7 @@ func get_name():
 func _enter_tree():
     import_plugin = load("res://addons/ComChibifireFbxImporter/ComChibifireFbxImporter.gdns").new()
     add_scene_import_plugin(import_plugin)
-    get_editor_interface().get_resource_filesystem().call_deferred("scan")	
 
 func _exit_tree():
     remove_scene_import_plugin(import_plugin)
-    get_editor_interface().get_resource_filesystem().call_deferred("scan")	
     import_plugin = null
