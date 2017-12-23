5432084e884b086dea52c62c9930d3e0c0c6f1e6
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-12-23 08:09:25 -0800

    Make build script fail on failure.

--- build_dependencies.bat
+++ build_dependencies.bat
@@ -1,10 +1,3 @@
 set current=%cd%
 set godot=%current%\thirdparty\godot
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
-cd /D %godot%
-call scons p=windows target=debug -j%NUMBER_OF_PROCESSORS% use_lto=yes gdnative_wrapper=yes
-cd /D %current%
-call %godot%/bin/godot.windows.tools.64.exe --gdnative-generate-json-api thirdparty/cpp_bindings/godot_api.json
-cd /D thirdparty/cpp_bindings
-call scons godotbinpath="%godot%/bin/godot.windows.tools.64.exe" headers="%godot%/modules/gdnative/include" p=windows generate_bindings=yes target=release
-cd /D %current%
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64 && cd /D %godot% &&call scons p=windows target=debug -j%NUMBER_OF_PROCESSORS% use_lto=yes gdnative_wrapper=yes vsproj=yes deprecated=no && cd /D %current% && call %godot%/bin/godot.windows.tools.64.exe --gdnative-generate-json-api thirdparty/cpp_bindings/godot_api.json && cd /D thirdparty/cpp_bindings && call scons godotbinpath="%godot%/bin/godot.windows.tools.64.exe" headers="%godot%/modules/gdnative/include" p=windows generate_bindings=yes target=release && cd /D %current%
