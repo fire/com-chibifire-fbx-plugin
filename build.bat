d00b2d7cc355e298d8830c1d1868e1fd3558afe2
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-01-15 20:31:50 -0800

    Update plugin.

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-bazel build //:com_chibifire_fbx_importer.dll && robocopy bazel-bin sample\addons\com_chibifire_fbx_importer\bin\windows.64\ com_chibifire_fbx_importer.pdb && robocopy bazel-bin sample\addons\com_chibifire_fbx_importer\bin\windows.64\ com_chibifire_fbx_importer.dll
+bazel build //:com_chibifire_fbx_importer.dll
+robocopy bazel-bin sample\addons\com_chibifire_fbx_importer\bin\windows.64\ com_chibifire_fbx_importer.pdb
+robocopy bazel-bin sample\addons\com_chibifire_fbx_importer\bin\windows.64\ com_chibifire_fbx_importer.dll
