cc9f4ac554ec3fc347e14dbcf7a555352b115e21
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-09-09 09:38:23 -0700

    Update sample project for godot 3.1.

--- sample/addons/com_chibifire_fbx_importer/src/Godot.cpp
+++ sample/addons/com_chibifire_fbx_importer/src/Godot.cpp
@@ -3,15 +3,16 @@
 
 using namespace godot;
 
-extern "C" void godot_gdnative_init(godot_gdnative_init_options *options) {
-	Godot::gdnative_init(options);
+extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o) {
+	godot::Godot::gdnative_init(o);
 }
 
-extern "C" void godot_gdnative_terminate(godot_gdnative_terminate_options *options) {
-	Godot::gdnative_terminate(options);
+extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *o) {
+	godot::Godot::gdnative_terminate(o);
 }
 
-extern "C" void godot_nativescript_init(void *handle) {
-	Godot::nativescript_init(handle);
-	register_tool_class<ComChibifireFbxImporter>();
+extern "C" void GDN_EXPORT godot_nativescript_init(void *handle) {
+	godot::Godot::nativescript_init(handle);
+
+	godot::register_tool_class<ComChibifireFbxImporter>();
 }
